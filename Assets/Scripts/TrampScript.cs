896dd42ba2fdb0ba5ab9804944529c4c14f3eaf0
CoolTaff12 <cool_taff@yahoo.com>
2016-02-12 23:51:31 +0100

    Fixing Trampoline script and other

--- Assets/Scripts/TrampScript.cs
+++ Assets/Scripts/TrampScript.cs
@@ -3,13 +3,17 @@ using System.Collections;
 
 public class TrampScript : MonoBehaviour
 {
+
     public float manualforce;
     public PlayerTarget playerInfo;
-	public Rigidbody rb;
+    public UnityStandardAssets.Characters.FirstPerson.FirstPersonController warriorInfo;
+    public Rigidbody rb;
+    private float startTime;
 
     // Use this for initialization
-    void Start () {
-	
+    void Start ()
+    {
+        startTime = Time.time;
 	}
 	
 	// Update is called once per frame
@@ -20,10 +24,25 @@ public class TrampScript : MonoBehaviour
     {
         if (col.gameObject.tag == "Player")
         {
-            playerInfo = col.gameObject.GetComponent<PlayerTarget>();
-            Vector3 velocity = col.gameObject.GetComponent<Rigidbody>().velocity;
+            float NewPosition = col.gameObject.transform.position.y + manualforce;
+            Vector3 Before = col.gameObject.transform.position;
+            Vector3 After = new Vector3 (col.gameObject.transform.position.x, NewPosition, gameObject.transform.position.z);
+            float fracComplete = (Time.time - startTime) / 1.0f;
+            col.gameObject.transform.position = Vector3.Lerp(Before, After, fracComplete);
+
+
+
+
+
+              /*  = new Vector3(col.gameObject.transform.position.x,
+                Mathf.Lerp(col.gameObject.transform.position.y, NewPosition, 50 * Time.deltaTime),
+                col.gameObject.transform.position.z);
+            manualforce--;*/
+            //playerInfo = col.gameObject.GetComponent<PlayerTarget>();
+           // warriorInfo = col.gameObject.GetComponent<UnityStandardAssets.Characters.FirstPerson.FirstPersonController>();
+        /*    Vector3 velocity = col.gameObject.GetComponent<Rigidbody>().velocity;
 			rb = col.gameObject.GetComponent<Rigidbody> ();
-			rb.velocity = new Vector3(rb.velocity.x, manualforce, rb.velocity.z);
+			rb.velocity = new Vector3(rb.velocity.x, manualforce, rb.velocity.z);*/
          //   rb.AddForce(col.gameObject.transform.up * manualforce);         
         }
     }
