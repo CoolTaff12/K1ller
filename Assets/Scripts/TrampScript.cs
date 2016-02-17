3b24249f545963d31dfe1f945fa1b4ec74f38180
CoolTaff12 <cool_taff@yahoo.com>
2016-02-17 16:31:57 +0100

    Chinese Scene + New Textures + TrampScript

--- Assets/Scripts/TrampScript.cs
+++ Assets/Scripts/TrampScript.cs
@@ -1,4 +1,5 @@
 ﻿using UnityEngine;
+using UnityStandardAssets.Utility;
 using System.Collections;
 
 public class TrampScript : MonoBehaviour
@@ -7,8 +8,16 @@ public class TrampScript : MonoBehaviour
     public float manualforce;
     public PlayerTarget playerInfo;
     public UnityStandardAssets.Characters.FirstPerson.FirstPersonController warriorInfo;
+    [SerializeField]
+    private LerpControlledBob Force = new LerpControlledBob();
     public Rigidbody rb;
     private float startTime;
+
+
+    public GameObject Warrior;
+    Vector3 Before;
+    Vector3 After;
+    float NewPosition;
 
     // Use this for initialization
     void Start ()
@@ -17,33 +26,44 @@ public class TrampScript : MonoBehaviour
 	}
 	
 	// Update is called once per frame
-	void Update () {
-	
+	void Update ()
+    {
+        if(Warrior != null)
+        {
+            NewPosition = Warrior.transform.position.y + manualforce;
+            After = new Vector3(Warrior.transform.position.x, NewPosition, Warrior.transform.position.z);
+            float fracComplete = (Time.time - startTime) / 1.0f;
+            Warrior.transform.position = Vector3.Lerp(Before, After, fracComplete);
+            if(Warrior.transform.position.y > manualforce)
+            {
+                Warrior = null;
+                //Warrior.transform.position = Vector3.Lerp(After, Before, (fracComplete / 2));
+            }
+          //  Warrior = null;
+        }
 	}
+
     void OnTriggerEnter(Collider col)
     {
         if (col.gameObject.tag == "Player")
         {
-            float NewPosition = col.gameObject.transform.position.y + manualforce;
-            Vector3 Before = col.gameObject.transform.position;
-            Vector3 After = new Vector3 (col.gameObject.transform.position.x, NewPosition, gameObject.transform.position.z);
-            float fracComplete = (Time.time - startTime) / 1.0f;
-            col.gameObject.transform.position = Vector3.Lerp(Before, After, fracComplete);
+            Before = col.gameObject.transform.position;
+            Warrior = col.gameObject;
 
 
 
 
 
-              /*  = new Vector3(col.gameObject.transform.position.x,
-                Mathf.Lerp(col.gameObject.transform.position.y, NewPosition, 50 * Time.deltaTime),
-                col.gameObject.transform.position.z);
-            manualforce--;*/
+            /*  = new Vector3(col.gameObject.transform.position.x,
+              Mathf.Lerp(col.gameObject.transform.position.y, NewPosition, 50 * Time.deltaTime),
+              col.gameObject.transform.position.z);
+          manualforce--;*/
             //playerInfo = col.gameObject.GetComponent<PlayerTarget>();
-           // warriorInfo = col.gameObject.GetComponent<UnityStandardAssets.Characters.FirstPerson.FirstPersonController>();
-        /*    Vector3 velocity = col.gameObject.GetComponent<Rigidbody>().velocity;
-			rb = col.gameObject.GetComponent<Rigidbody> ();
-			rb.velocity = new Vector3(rb.velocity.x, manualforce, rb.velocity.z);*/
-         //   rb.AddForce(col.gameObject.transform.up * manualforce);         
+            // warriorInfo = col.gameObject.GetComponent<UnityStandardAssets.Characters.FirstPerson.FirstPersonController>();
+            /*    Vector3 velocity = col.gameObject.GetComponent<Rigidbody>().velocity;
+                rb = col.gameObject.GetComponent<Rigidbody> ();
+                rb.velocity = new Vector3(rb.velocity.x, manualforce, rb.velocity.z);*/
+            //   rb.AddForce(col.gameObject.transform.up * manualforce);         
         }
     }
 }
