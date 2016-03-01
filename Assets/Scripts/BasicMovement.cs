7d874c42c83b5d17e693b1b77ef0d8e63c2438b3
ExVeeDoubleU <thexvw@gmail.com>
2016-03-02 00:33:17 +0100

    Parkour Detection, basic movement, parkour testing scene.

---
+++ Assets/Scripts/BasicMovement.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BasicMovement : MonoBehaviour {
+	Rigidbody rb;
+	public float speed = 2;
+
+	// Use this for initialization
+	void Start () {
+		rb = gameObject.GetComponent<Rigidbody> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKey (KeyCode.W)) {
+			rb.velocity = new Vector3 (rb.velocity.y, rb.velocity.y, speed);
+		}
+		if (Input.GetKey (KeyCode.S)) {
+			rb.velocity = new Vector3 (rb.velocity.y, rb.velocity.y, -speed);
+		}
+		if (Input.GetKey (KeyCode.A)) {
+			rb.velocity = new Vector3 (speed, rb.velocity.y, rb.velocity.z);
+		}
+		if (Input.GetKey (KeyCode.D)) {
+			rb.velocity = new Vector3 (-speed, rb.velocity.y, rb.velocity.z);
+		}
+	
+	}
+}
