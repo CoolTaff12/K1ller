d521320f6023c2cb8e42a582fec5e127be281ff3
ExVeeDoubleU <thexvw@gmail.com>
2016-03-02 17:55:27 +0100

    Fixed Multiple Cameras

---
+++ Assets/Scripts/PlayerNetworkBehavior.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.Networking;
+
+public class PlayerNetworkBehavior : NetworkBehaviour {
+
+	public Camera cam;
+	// Use this for initialization
+	void Start () {
+		if (isLocalPlayer) return;
+
+		cam.enabled = false;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
