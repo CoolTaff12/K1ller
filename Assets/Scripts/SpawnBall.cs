55c6e809f6f0053147631a4ca34a9a265b69fd3d
hermieboi <herman_cool@hotmail.com>
2016-03-14 11:57:49 +0100

    no message

---
+++ Assets/Scripts/SpawnBall.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.Networking;
+
+public class SpawnBall : NetworkBehaviour {
+	public GameObject ballPrefab;
+
+	// Use this for initialization
+	void Start () {
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+	void OnTriggerEnter(Collider col){
+		if (col.tag == "Player"){
+			GameObject SpeedingBall = Instantiate(ballPrefab, transform.position, Quaternion.identity) as GameObject;
+			NetworkServer.Spawn(SpeedingBall);	
+		}
+	}
+}
