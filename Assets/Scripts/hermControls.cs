3d58c877e605353046bb30b592e7a3cb16b3899e
hermieboi <herman_cool@hotmail.com>
2016-03-29 13:24:34 +0200

    Animations & shit

--- Assets/Scripts/hermControls.cs
+++ Assets/Scripts/hermControls.cs
@@ -1,14 +1,14 @@
 ﻿using UnityEngine;
 using System.Collections;
+using UnityEngine.Networking;
 
-public class hermControls : MonoBehaviour 
+public class hermControls : NetworkBehaviour 
 {
 	public Animator anim;
 	public float speed = 10.0F; 
 	public float rotationSpeed = 100.0F; 
 	public testThrowBall testTB;
-	public Rigidbody character;
-	public GameObject character2;
+
 
 	public bool isOnground;
 	public Transform groundCheck;
@@ -30,33 +30,26 @@ public class hermControls : MonoBehaviour
 
 	void Update () 
 	{ 
-		if (isOnground && Input.GetButtonDown("Jump"))
+		if(!isLocalPlayer)
 		{
-			GetComponent<Rigidbody>().AddForce(new Vector3(0, jumpForce, 0));
-			maxJumpTimeInternal = maxJumpTime;
-			anim.SetTrigger("isJumping");
-		}
+			return;
 
-		if(Input.GetButton("Jump") && GetComponent<Rigidbody>().velocity.y > 1 && maxJumpTimeInternal > 0)
-		{
-			GetComponent<Rigidbody>().AddForce(new Vector3(0, jumpForceHold, 0));
-			maxJumpTimeInternal = maxJumpTimeInternal -1;
 		}
+			
+		Move();
+		Jump();
+		Throw();
+
+	}
+
+	void Move()
+	{
 		float translation = Input.GetAxis("Vertical") * speed; 
 		float rotation = Input.GetAxis("Horizontal") * rotationSpeed; 
 		translation *= Time.deltaTime;
 		rotation *= Time.deltaTime; 
 		transform.Translate(0, 0, translation); 
 		transform.Rotate(0, rotation, 0);
-
-
-		if (Input.GetKeyDown(KeyCode.Mouse0))
-		{
-			anim.SetTrigger("isThrowing");
-			testTB.ThrowBall();
-
-
-		}
 
 
 		if (translation != 0)
@@ -70,9 +63,36 @@ public class hermControls : MonoBehaviour
 			anim.SetBool("isIdle", true);
 		}
 
+	}
 
+
+	void Jump()
+	{
+		if (isOnground && Input.GetButtonDown("Jump"))
+		{
+			GetComponent<Rigidbody>().AddForce(new Vector3(0, jumpForce, 0));
+			maxJumpTimeInternal = maxJumpTime;
+			anim.SetTrigger("isJumping");
+		}
+
+		if(Input.GetButton("Jump") && GetComponent<Rigidbody>().velocity.y > 1 && maxJumpTimeInternal > 0)
+		{
+			GetComponent<Rigidbody>().AddForce(new Vector3(0, jumpForceHold, 0));
+			maxJumpTimeInternal = maxJumpTimeInternal -1;
+		}
+	}
+
+
+	void Throw()
+	{
+		if (Input.GetKeyDown(KeyCode.Mouse0))
+		{
+			anim.SetTrigger("isThrowing");
+			testTB.ThrowBall();
+		}
 
 	}
+
 
 	void FixedUpdate()
 	{
