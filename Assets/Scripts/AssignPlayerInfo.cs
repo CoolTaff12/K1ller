f38835c38dface5e50a932baf9b0decf2d53afa1
CoolTaff12 <cool_taff@yahoo.com>
2016-04-05 16:23:45 +0200

    Pissing Max Off ;D

--- Assets/Scripts/AssignPlayerInfo.cs
+++ Assets/Scripts/AssignPlayerInfo.cs
@@ -19,23 +19,4 @@ public class AssignPlayerInfo : NetworkBehaviour {
 //	{
 //		Rpc_SetTeamNumber(go);
 //	}
-	[ClientRpc]
-	public void Rpc_SetTeamNumber(GameObject go){
-		go.GetComponent<GrabAndToss> ().teamNumber = teamNumber;
-		teamNumber++;
-	}
-	[ClientRpc]
-	public void Rpc_SetName(GameObject go){
-		go.transform.name = "Player" + teamNumber;
-	}
-	[ClientRpc]
-	public void Rpc_KillAPlayer(GameObject go){
-		go.GetComponent<GrabAndToss> ().dead = true;
-		go.GetComponent<GrabAndToss> ().teamNumber = 0;
-		go.transform.localScale = new Vector3 (0.2f, 0.2f, 0.2f);
-		Rigidbody rb = go.GetComponent<Rigidbody> ();
-		rb.detectCollisions = false;
-		rb.useGravity = false;
-		
-	}
 }
--- Assets/Scripts/NetworkScripts/NetworkCharacterInfo.cs
+++ Assets/Scripts/NetworkScripts/NetworkCharacterInfo.cs
@@ -11,7 +11,7 @@ public class NetworkCharacterInfo : NetworkBehaviour
 {
 
     //Network syncvar
-    [SyncVar(hook = "OnScoreChanged")]
+   [SyncVar(hook = "OnScoreChanged")]
     public int score;
     [SyncVar]
     public Color color;
@@ -21,6 +21,18 @@ public class NetworkCharacterInfo : NetworkBehaviour
     public int lifeCount;
     [SyncVar]
     public int teamNumber = 1;
+
+    protected Text ScoreText;
+    //hard to control WHEN Init is called (networking make order between object spawning non deterministic)
+    //so we call init from multiple location (depending on what between spaceship & manager is created first).
+    protected bool WasInit = false;
+
+
+    void Awake()
+    {
+        //register the spaceship in the gamemanager, that will allow to loop on it.
+        NetworkGameManager.sCHaracter.Add(this);
+    }
 
     // Use this for initialization
     void Start ()
@@ -43,5 +55,59 @@ public class NetworkCharacterInfo : NetworkBehaviour
     {
         go.GetComponent<GrabAndToss>().teamNumber = teamNumber;
         teamNumber++;
+    }
+    [ClientRpc]
+    public void Rpc_SetName(GameObject go)
+    {
+        go.transform.name = "Player" + teamNumber;
+    }
+    [ClientRpc]
+    public void Rpc_KillAPlayer(GameObject go)
+    {
+        go.GetComponent<GrabAndToss>().dead = true;
+        go.GetComponent<GrabAndToss>().teamNumber = 0;
+        go.transform.localScale = new Vector3(0.2f, 0.2f, 0.2f);
+        Rigidbody rb = go.GetComponent<Rigidbody>();
+        rb.detectCollisions = false;
+        rb.useGravity = false;
+    }
+
+    public void Init()
+    {
+        if (WasInit)
+            return;
+
+        GameObject scoreGO = new GameObject(playerName + "score");
+        scoreGO.transform.SetParent(NetworkGameManager.sInstance.uiScoreZone.transform, false);
+        ScoreText = scoreGO.AddComponent<Text>();
+        ScoreText.alignment = TextAnchor.MiddleCenter;
+        ScoreText.font = NetworkGameManager.sInstance.uiScoreFont;
+        ScoreText.resizeTextForBestFit = true;
+        ScoreText.color = color;
+        WasInit = true;
+
+        UpdateScoreLifeText();
+    }
+    // --- Score & Life management & display
+    void OnScoreChanged(int newValue)
+    {
+        score = newValue;
+        UpdateScoreLifeText();
+    }
+
+    void OnLifeChanged(int newValue)
+    {
+        lifeCount = newValue;
+        UpdateScoreLifeText();
+    }
+
+    void UpdateScoreLifeText()
+    {
+        if (ScoreText != null)
+        {
+            ScoreText.text = playerName + "\nSCORE : " + score + "\nLIFE : ";
+            for (int i = 1; i <= lifeCount; ++i)
+                ScoreText.text += "X";
+        }
     }
 }
