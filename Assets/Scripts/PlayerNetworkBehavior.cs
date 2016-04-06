5dd2b3f8ce49e602d8eecd69171a19ef263fbd1b
ExVeeDoubleU <thexvw@gmail.com>
2016-04-06 04:33:37 +0200

    Made animations compatible with FirstPersonController. Syncing of Animations.

--- Assets/Scripts/PlayerNetworkBehavior.cs
+++ Assets/Scripts/PlayerNetworkBehavior.cs
@@ -4,25 +4,57 @@ using UnityEngine.Networking;
 
 public class PlayerNetworkBehavior : NetworkBehaviour {
 
-	public Camera cam;
-	public GrabAndToss gat;
+	[SerializeField] Camera cam;
+	[SerializeField] GrabAndToss gat;
+	[SerializeField] AudioListener audioLis;
+	[SerializeField] GameObject[] bodyParts;
+
+
+	public override void OnStartLocalPlayer ()
+	{
+		cam.enabled = true;
+		gat = gameObject.GetComponent<GrabAndToss> ();
+		gat.enabled = true;
+		audioLis.enabled = true;
+		foreach (GameObject go in bodyParts) {
+							go.layer = 9;
+						}
+
+		GetComponent<NetworkAnimator> ().SetParameterAutoSend (0, true);
+		GetComponent<NetworkAnimator> ().SetParameterAutoSend (1, true);
+		GetComponent<NetworkAnimator> ().SetParameterAutoSend (2, true);
+		GetComponent<NetworkAnimator> ().SetParameterAutoSend (3, true);
+		GetComponent<NetworkAnimator> ().SetParameterAutoSend (4, true);
+	}
+
+	public override void PreStartClient(){
+		GetComponent<NetworkAnimator> ().SetParameterAutoSend (0, true);
+		GetComponent<NetworkAnimator> ().SetParameterAutoSend (1, true);
+		GetComponent<NetworkAnimator> ().SetParameterAutoSend (2, true);
+		GetComponent<NetworkAnimator> ().SetParameterAutoSend (3, true);
+		GetComponent<NetworkAnimator> ().SetParameterAutoSend (4, true);
+
+	}
+
 //	public PlayerTarget playerTarget;
 	// Use this for initialization
-	void Start () {
-		if (isLocalPlayer) {
-			gat = gameObject.GetComponent<GrabAndToss> ();
-//			playerTarget = gameObject.GetComponent <PlayerTarget> ();
-
-			gat.enabled = true;
-//			playerTarget.enabled = true;
-		} else {
-			cam.enabled = false;
-			return;
-		}
-	}
-	
-	// Update is called once per frame
-	void Update () {
-	
-	}
+//	void Start () {
+//		if (isLocalPlayer) {
+//			gat = gameObject.GetComponent<GrabAndToss> ();
+////			playerTarget = gameObject.GetComponent <PlayerTarget> ();
+//			gat.enabled = true;
+//			foreach (GameObject go in bodyParts) {
+//				go.layer = 9;
+//			}
+////			playerTarget.enabled = true;
+//		} else {
+//			cam.enabled = false;
+//			return;
+//		}
+//	}
+//	
+//	// Update is called once per frame
+//	void Update () {
+//	
+//	}
 }
