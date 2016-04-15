26b821e7d49c5cc8db1e9a092af69cc946ea7f4f
CoolTaff12 <cool_taff@yahoo.com>
2016-04-15 12:54:22 +0200

    Fixing Errors Part 2

--- Assets/Scripts/Edited Scripts/Trampoline.cs
+++ Assets/Scripts/Edited Scripts/Trampoline.cs
@@ -1,13 +1,3 @@
-/*
- * Copyright (C) 2015 Bun Bun Studios.
- *
- * This software is distributed to you as-is without any guarantees or warranties of any kind. You agree 
- * to use this software at your own risk. Bun Bun Studios is not liable for anything directly or indirectly. You 
- * are not allowed to copy, distribute, redistribute, sell, resell, reproduce or transfer this software or 
- * derivations of this software.
- * 
- */
-
 using UnityEngine;
 using System.Collections;
 
